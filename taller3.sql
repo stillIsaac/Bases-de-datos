e222b8e49d4d1c7d40ba84f98c15ad19021cd29f
stillIsaac <95540706+stillIsaac@users.noreply.github.com>
2023-08-11 12:29:53 -0500

    Update taller3.sql

--- taller3.sql
+++ taller3.sql
@@ -1,7 +1,1 @@
---Primera consulta
-SELECT I.name FROM instructor I WHERE I.salary BETWEEN 100000 AND 115000;
---Segunda consulta
-SELECT DISTINCT S.name
-FROM takes T join student S on t.id = s.id
-WHERE T.grade = 'A+';
---Tercera consulta
+
